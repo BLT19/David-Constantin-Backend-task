b4180a016ebb3dfc540f84b99810cf7bda46efd6
BLT19 <david.constantin06@gmail.com>
2025-11-19 15:00:25 +0000

    Made the city

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
 from app import app
+import pandas as pd
+import json
 
+table = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
 @app.route('/')
 def index():
-    return "Index page"
+    return table.to_json(orient='records')
+
+@app.route('/city/<item>')
+def city(item):
+    sightings = table[table['location'] == item]
+
+    arr = sightings.to_dict(orient='records')
+    
+    return json.dumps(arr)
+   
