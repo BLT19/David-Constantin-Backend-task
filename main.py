fef345c464687ee64b3c97c01d4b243330edfaa7
BLT19 <david.constantin06@gmail.com>
2025-11-19 01:03:56 +0000

    added excel file and started implementing functionality

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from app import app
+import pandas as pd
+import datetime
+
+table = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
+
+print(table)
