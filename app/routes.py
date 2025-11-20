ad7f38f462adbfad280a559d2bcea69d9f79913e
BLT19 <david.constantin06@gmail.com>
2025-11-20 17:45:27 +0000

    Improved location filtering, started working on error handling

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,40 @@
 from app import app
 import pandas as pd
 import json
+from datetime import datetime
+import re
+import werkzeug.exceptions
 
 table = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
+
 @app.route('/')
 def index():
-    return table.to_json(orient='records')
+    return json.dumps(table.to_dict(orient='records'))
 
+@app.route('/city', defaults={'item': None})
 @app.route('/city/<item>')
 def city(item):
-    sightings = table[table['location'] == item]
-
-    arr = sightings.to_dict(orient='records')
+    def use_regex(input_item):
+        pattern = re.compile(r"[A-Z][a-z]+")
+        return pattern.match(input_item)
     
-    return json.dumps(arr)
-   
+    if use_regex(item):
+        if item:
+            sightings = table[table['location'] == item]
+
+            arr = sightings.to_dict()
+            total_entries = sightings.shape[0]
+            result = {'total_entries': total_entries, 'data': arr}
+    
+            return json.dumps(result)
+    
+        else:
+            return 0
+        
+    else:
+        return bad_request
+
+#@app.route('/date/<after> <before')
+#def date(after, before):
+#    if before is None and after is None:
+#        sightings = table[table['']]
