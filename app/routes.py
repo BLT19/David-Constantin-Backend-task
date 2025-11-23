fbbcb0718d84eac658e7c84616f2bf9662f7ca02
BLT19 <david.constantin06@gmail.com>
2025-11-23 17:30:20 +0000

    Implemented monthly trends

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,15 @@ from datetime import datetime
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import calendar
+from matplotlib.figure import Figure
+import base64
+from io import BytesIO
 
 df = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
 df['date'] = pd.to_datetime(df['date'])
+global_monthly_max_entries = df.groupby([df['date'].dt.year, df['date'].dt.month]).size().max()
+y_limit = global_monthly_max_entries + 10
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -18,15 +24,15 @@ def index():
     return df.to_json(orient='records', date_format='iso')
 
 @app.route('/city', defaults={'item': None})
-@app.route('/city/<item>')
-def city(item):
+@app.route('/city/<city>')
+def city(city):
     def use_regex(input_item):
         pattern = re.compile(r"[A-Z][a-z]+")
         return pattern.match(input_item)
     
-    if item:
-        if use_regex(item):
-            sightings = df[df['location'] == item]
+    if city:
+        if use_regex(city):
+            sightings = df[df['location'] == city]
 
             total_entries = sightings.shape[0]
             result = {'total_entries': total_entries, 'data': sightings.to_json(orient='records', date_format='iso')}
@@ -77,9 +83,68 @@ def date(after, before):
             return bad_request('Invalid input (Dates must be in the format YYYY-mm-ddThh:mm:ss)')
     else:
         return df.to_json(orient='records', date_format='iso')
-    
-@app.route('/trend/weekly/year=<year>month=<month>')
-def weekly(year, month):
-    def use_regex(input_item):
+
+@app.route('/trend/monthly', defaults={'year':None, 'city':None}) 
+@app.route('/trend/monthly/year=<year>', defaults={'city':None})
+@app.route('/trend/monthly/year=<year>city=<city>')
+def monthly(year, city):
+    def year_regex(input_item):
         pattern = re.compile(r"^[0-9]{4}$")
         return pattern.match(input_item)
+    
+    def city_regex(input_item):
+        pattern = re.compile(r"[A-Z][a-z]+")
+        return pattern.match(input_item)
+
+    if year and city:
+        if year_regex(year) and city_regex(city):
+            year = int(year)
+            temp = df[df['date'].dt.year == year]
+            sightings = temp[temp['location']== city]
+            monthly_counts = sightings.groupby(sightings['date'].dt.month).size()
+            monthly_counts = monthly_counts.reindex(range(1, 13), fill_value=0)
+            monthly_counts.index = [calendar.month_abbr[x] for x in monthly_counts.index]
+
+            fig, ax = plt.subplots()
+            monthly_counts.plot(kind='bar', ax=ax, title=f'Trend for {year} in {city}', rot=0)
+            ax.set_xlabel("Month")
+            ax.set_ylabel("Entries")
+            ax.set_ylim(0, y_limit)
+
+            buf = BytesIO()
+            fig.savefig(buf, format='png')
+    
+            plot_url = base64.b64encode(buf.getbuffer()).decode()
+    
+            plt.close(fig) 
+            return f'<img src="data:image/png;base64,{plot_url}"/>'
+        
+        else:
+            return bad_request('Please input a valid year and, if you would like to filter results further, a valid city (City names must be capitalised)')
+        
+    elif year:
+        if year_regex(year):
+            year = int(year)
+            sightings = df[df['date'].dt.year == year]
+            monthly_counts = sightings.groupby(sightings['date'].dt.month).size()
+            monthly_counts = monthly_counts.reindex(range(1, 13), fill_value=0)
+            monthly_counts.index = [calendar.month_abbr[x] for x in monthly_counts.index]
+
+            fig, ax = plt.subplots()
+            monthly_counts.plot(kind='bar', ax=ax, title=f'Trend for {year}', rot=0)
+            ax.set_xlabel("Month")
+            ax.set_ylabel("Entries")
+            ax.set_ylim(0, y_limit)
+
+            buf = BytesIO()
+            fig.savefig(buf, format='png')
+    
+            plot_url = base64.b64encode(buf.getbuffer()).decode()
+    
+            plt.close(fig) 
+            return f'<img src="data:image/png;base64,{plot_url}"/>'
+        else:
+            return bad_request('Please input a valid year and, if you would like to filter results further, a valid city (City names must be capitalised)')
+        
+    else:
+        return bad_request('Please input a valid year and, if you would like to filter results further, a valid city (City names must be capitalised)')
