240b30e3fde62ae05205f2218d990cc39a5912a3
BLT19 <david.constantin06@gmail.com>
2025-11-23 18:27:11 +0000

    Added weekly trends

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,11 @@ from io import BytesIO
 
 df = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
 df['date'] = pd.to_datetime(df['date'])
-global_monthly_max_entries = df.groupby([df['date'].dt.year, df['date'].dt.month]).size().max()
-y_limit = global_monthly_max_entries + 10
+global_monthly_max_sightings = df.groupby([df['date'].dt.year, df['date'].dt.month]).size().max()
+monthly_y_limit = global_monthly_max_sightings + 10
+
+global_weekly_max_sightings = df.groupby([df['date'].dt.year, df['date'].dt.isocalendar().week]).size()
+weekly_y_limit = global_weekly_max_sightings.max() + 10
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -106,10 +109,10 @@ def monthly(year, city):
             monthly_counts.index = [calendar.month_abbr[x] for x in monthly_counts.index]
 
             fig, ax = plt.subplots()
-            monthly_counts.plot(kind='bar', ax=ax, title=f'Trend for {year} in {city}', rot=0)
+            monthly_counts.plot(kind='bar', ax=ax, title=f'Monthly trend for {year} in {city}', rot=0)
             ax.set_xlabel("Month")
-            ax.set_ylabel("Entries")
-            ax.set_ylim(0, y_limit)
+            ax.set_ylabel("Sightings")
+            ax.set_ylim(0, monthly_y_limit)
 
             buf = BytesIO()
             fig.savefig(buf, format='png')
@@ -131,10 +134,10 @@ def monthly(year, city):
             monthly_counts.index = [calendar.month_abbr[x] for x in monthly_counts.index]
 
             fig, ax = plt.subplots()
-            monthly_counts.plot(kind='bar', ax=ax, title=f'Trend for {year}', rot=0)
+            monthly_counts.plot(kind='bar', ax=ax, title=f'Monthly trend for {year}', rot=0)
             ax.set_xlabel("Month")
-            ax.set_ylabel("Entries")
-            ax.set_ylim(0, y_limit)
+            ax.set_ylabel("Sightings")
+            ax.set_ylim(0, monthly_y_limit)
 
             buf = BytesIO()
             fig.savefig(buf, format='png')
@@ -144,7 +147,89 @@ def monthly(year, city):
             plt.close(fig) 
             return f'<img src="data:image/png;base64,{plot_url}"/>'
         else:
-            return bad_request('Please input a valid year and, if you would like to filter results further, a valid city (City names must be capitalised)')
+            return bad_request('Please input a valid year and (optional) a valid city (City names must be capitalised)')
         
     else:
-        return bad_request('Please input a valid year and, if you would like to filter results further, a valid city (City names must be capitalised)')
+        return bad_request('Please input a valid year and (optional) a valid city (City names must be capitalised)')
+    
+@app.route('/trend/weekly', defaults={'year':None, 'month':None, 'city':None})
+@app.route('/trend/weekly/year=<year>month=<month>', defaults={'city':None})
+@app.route('/trend/weekly/year=<year>month=<month>city=<city>')
+def weekly(year, month, city):
+    def year_regex(input_item):
+        pattern = re.compile(r"^[0-9]{4}$")
+        return pattern.match(input_item)
+    
+    def month_regex(input_item):
+        pattern = re.compile(r"^[0-9]{2}$")
+        return pattern.match(input_item)
+    
+    def city_regex(input_item):
+        pattern = re.compile(r"[A-Z][a-z]+")
+        return pattern.match(input_item)
+    
+    if year and month and city:
+        if year_regex(year) and month_regex(month) and city_regex(city):
+            year = int(year)
+            month = int(month)
+            temp1 = df[df['date'].dt.year == year]
+            temp2 = temp1[temp1['date'].dt.month == month]
+            sightings = temp2[temp2['location']== city]
+
+            start_date = pd.Timestamp(year, month, 1)
+            end_date = start_date + pd.offsets.MonthEnd()
+            weeks_in_month = pd.date_range(start=start_date, end=end_date).isocalendar().week.unique()
+
+            weekly_counts = sightings.groupby(sightings['date'].dt.isocalendar().week).size()
+            weekly_counts = weekly_counts.reindex(weeks_in_month, fill_value=0)
+
+            fig, ax = plt.subplots()
+            weekly_counts.plot(kind='bar', ax=ax, title=f'Weekly Trend for {month}/{year} in {city}', rot=0)
+    
+            ax.set_xlabel("Week")
+            ax.set_ylabel("Sightings")
+            ax.set_ylim(0, weekly_y_limit)
+
+            img = BytesIO()
+            fig.savefig(img, format='png')
+            plt.close(fig)
+
+            plot_url = base64.b64encode(img.getvalue()).decode()
+            return f'<img src="data:image/png;base64,{plot_url}"/>'
+        
+        else:
+            return bad_request('Please input a valid year, month, and (optional) a city (City names must be capitalised)')
+        
+    elif year and month:
+        if year_regex(year) and month_regex(month):
+            year = int(year)
+            month = int(month)
+            temp = df[df['date'].dt.year == year]
+            sightings = temp[temp['date'].dt.month == month]
+
+            start_date = pd.Timestamp(year, month, 1)
+            end_date = start_date + pd.offsets.MonthEnd()
+            weeks_in_month = pd.date_range(start=start_date, end=end_date).isocalendar().week.unique()
+
+            weekly_counts = sightings.groupby(sightings['date'].dt.isocalendar().week).size()
+            weekly_counts = weekly_counts.reindex(weeks_in_month, fill_value=0)
+
+            fig, ax = plt.subplots()
+            weekly_counts.plot(kind='bar', ax=ax, title=f'Weekly Trend for {month}/{year}', rot=0)
+    
+            ax.set_xlabel("Week number")
+            ax.set_ylabel("Sightings")
+            ax.set_ylim(0, weekly_y_limit)
+
+            img = BytesIO()
+            fig.savefig(img, format='png')
+            plt.close(fig)
+
+            plot_url = base64.b64encode(img.getvalue()).decode()
+            return f'<img src="data:image/png;base64,{plot_url}"/>'
+        
+        else:
+            return bad_request('Please input a valid year, month, and (optional) a city (City names must be capitalised)')
+    
+    else:
+        return bad_request('Please input a valid year, month, and (optional) a city (City names must be capitalised)')
