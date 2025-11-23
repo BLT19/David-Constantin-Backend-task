c5f9271a8b2ed845669447b43e8a8912f05e41cc
BLT19 <david.constantin06@gmail.com>
2025-11-23 14:26:36 +0000

    Added sanitisation to date filtering + changed variable names for better practice

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,19 @@ import pandas as pd
 import json
 from datetime import datetime
 import re
+import matplotlib.pyplot as plt
+import numpy as np
 
-table = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
+df = pd.read_excel('Tick Sightings.xlsx', dtype={'id':str, 'date':str, 'location':str, 'species':str, 'latinName':str})
+df['date'] = pd.to_datetime(df['date'])
 
-@app.errorhandler(404)
+@app.errorhandler(400)
 def bad_request(error):
-    return error, 404
+    return error, 400
 
 @app.route('/')
 def index():
-    return table.to_json(orient='records')
+    return df.to_json(orient='records', date_format='iso')
 
 @app.route('/city', defaults={'item': None})
 @app.route('/city/<item>')
@@ -23,40 +26,58 @@ def city(item):
     
     if item:
         if use_regex(item):
-            sightings = table[table['location'] == item]
+            sightings = df[df['location'] == item]
 
             total_entries = sightings.shape[0]
-            result = {'total_entries': total_entries, 'data': sightings.to_json(orient='records')}
+            result = {'total_entries': total_entries, 'data': sightings.to_json(orient='records', date_format='iso')}
     
             return json.dumps(result)
         else:
             return bad_request('Invalid input (The first letter of the city must be capitalised)')
     else:
-        return table.to_json(orient='records')
+        return df.to_json(orient='records',date_format='iso')
 
 @app.route('/date', defaults={'after': None, 'before': None})
 @app.route('/date/after=<after>', defaults={'before': None})
 @app.route('/date/before=<before>', defaults={'after': None})
 @app.route('/date/after=<after>before=<before>')
 def date(after, before):
-    table['date'] = pd.to_datetime(table['date'])
+    def use_regex(input_item):
+        pattern = re.compile(r"[0-9]+-(0[1-9]|1[0,1,2])-(0[1-9]|[12][0-9]|3[01])T(0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9]):(0?[0-9]|[1-5][0-9])")
+        return pattern.match(input_item)
+
     if after and before:
-        after = pd.to_datetime(after)
-        before = pd.to_datetime(before)
+        if use_regex(after) and use_regex(before):
+            after = pd.to_datetime(after)
+            before = pd.to_datetime(before)
 
-        temp = table[table['date'] > after]
-        sightings = temp[temp['date'] < before]
-        return sightings.to_json(orient='records', date_format='iso')
+            temp = df[df['date'] > after]
+            sightings = temp[temp['date'] < before]
+            return sightings.to_json(orient='records', date_format='iso')
+        
+        else:
+            return bad_request('Invalid input (Dates must be in the format YYYY-mm-ddThh:mm:ss)')
     elif after:
-        after = pd.to_datetime(after)
+        if use_regex(after):
+            after = pd.to_datetime(after)
 
-        sightings = table[table['date'] > after]
-        return sightings.to_json(orient='records', date_format='iso')
+            sightings = df[df['date'] > after]
+            return sightings.to_json(orient='records', date_format='iso')
+        
+        else:
+            return bad_request('Invalid input (Dates must be in the format YYYY-mm-ddThh:mm:ss)')
     elif before:
-        before = pd.to_datetime(before)
+        if use_regex(before):
+            before = pd.to_datetime(before)
 
-        sightings = table[table['date'] < before]
-        return sightings.to_json(orient='records', date_format='iso')
+            sightings = df[df['date'] < before]
+            return sightings.to_json(orient='records', date_format='iso')
+        
+        else:
+            return bad_request('Invalid input (Dates must be in the format YYYY-mm-ddThh:mm:ss)')
     else:
-        table['date'] = table['date'].dt.strftime('%Y-%m-%dT%H:%M:%S')
-        return table.to_json(orient='records')
+        return df.to_json(orient='records', date_format='iso')
+    
+@app.route('/trend/weekly/year=<year>month=<month>')
+def weekly(year, month):
+    return 0
